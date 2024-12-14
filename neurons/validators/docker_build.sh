c712f038629bd6f9c385f62d8675cb04caf296f6
CoinfarmVentures <rohith@coinfarm.us>
2024-12-14 17:44:45 -0500

    asfsf

--- neurons/validators/docker_build.sh
+++ neurons/validators/docker_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
 
+TAG=latest
+
 IMAGE_NAME="daturaai/compute-subnet-validator:$TAG"
 
 docker build --build-context datura=../../datura -t $IMAGE_NAME .
--- neurons/validators/docker_runner_build.sh
+++ neurons/validators/docker_runner_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eux -o pipefail
+TAG=latest
 
 IMAGE_NAME="daturaai/compute-subnet-validator-runner:$TAG"
 
