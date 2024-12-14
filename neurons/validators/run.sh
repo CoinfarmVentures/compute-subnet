1bd2c27d06496941ca768839d39024e6286ec0b1
CoinfarmVentures <rohith@coinfarm.us>
2024-12-14 18:43:35 -0500

    dbg1

--- neurons/validators/run.sh
+++ neurons/validators/run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-python -m debugpy --listen 0.0.0.0:5678 --wait-for-client &
-
 # db migrate
 alembic upgrade head
 
 # run fastapi app
-python src/validator.py
+python -m debugpy --listen 0.0.0.0:5678 --wait-for-client src/validator.py
