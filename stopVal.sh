da098bc6e54ed5c52143143702bbae283b073563
CoinfarmVentures <rohith@coinfarm.us>
2024-12-14 17:16:36 -0500

    waedsdws

--- stopVal.sh
+++ stopVal.sh
@@ -4,5 +4,7 @@ pm2 stop dbg_sn51val
 pm2 del dbg_sn51val
 cd /bittensor/compute-subnet/neurons/validators
 docker compose down
-docker stop validator-validator-1 -t 1
+docker stop validator-db-1 -t 1
+docker stop validator-connector-1 -t 1
+docker stop validator-redis-1 -t 1
 cd /bittensor
